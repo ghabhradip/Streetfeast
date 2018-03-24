ef60a534a2890475ab87f17e5d032e10659349e0
Abhradip Ghosh <ghabhradip@gmail.com>
2018-03-24 10:51:17 +0530

    dashboard partials added

--- app/controllers/management_controller.rb
+++
@@ -1,8 +0,0 @@
-class ManagementController < ApplicationController
-	def show_users
-		@users=User.all
-
-
-		render partial: "home/user_data"
-	end
-end
--- test/controllers/management_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ManagementControllerTest < ActionController::TestCase
-  test "should get show_users" do
-    get :show_users
-    assert_response :success
-  end
-
-end
