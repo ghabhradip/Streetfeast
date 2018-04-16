b929ec30c4eecbda467e230fc849e9004bb2e202
Rahil <rahil.brainium@gmail.com>
2018-04-16 20:01:34 +0530

    css changed for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ StreetFeast::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
